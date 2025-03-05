81a4f56607683b4aaea72e380af3a9b6584dbd5d
Sherwin Varghese <sherwin.varghese@neenopal.com>
2025-03-05 14:10:26 +0530

    removed print statments and created request template

--- src/connectors/survey_monkey.py
+++ src/connectors/survey_monkey.py
@@ -4,16 +4,12 @@ import httpx
 from dotenv import load_dotenv
 
 load_dotenv(os.path.join('..', 'config', '.env'))
-
-# Access the ACCESS_TOKEN
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
-print("ACCESS_TOKEN:", ACCESS_TOKEN)
 
-def get_me():
-    access_token = ACCESS_TOKEN
+def request_template():
     headers = {
         'Accept': "application/json",
-        'Authorization': f"Bearer {access_token}"
+        'Authorization': f"Bearer {ACCESS_TOKEN}"
     }
     response = httpx.get("https://api.surveymonkey.com/v3/users/me", headers=headers)
     if response.status_code == 200:
