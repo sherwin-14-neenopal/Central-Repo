4a5a672ea04c1f56129709d9dc2aa30d36210e47
Sherwin Varghese <sherwin.varghese@neenopal.com>
2025-03-08 13:25:49 +0530

    started with klaviyo

---
+++ src/connectors/klaviyo.py
@@ -0,0 +1,25 @@
+import os
+
+
+from dotenv import load_dotenv
+from typing import Optional
+from typing import Any
+from klaviyo_api import KlaviyoAPI
+
+# Loading api token from config dir
+load_dotenv("./src/config/.env")
+API_KEY_KLAVIYO = os.environ.get("API_KEY_KLAVIYO")
+
+def init_klaviyo() -> Any:
+    """
+    Authenticate with the Klaviyo API.
+
+    This function serves as a general template to initialize the Klaviyo API connection.
+    It requires an API key for authorization and returns a KlaviyoAPI instance.
+
+    Returns:
+        Any: The function returns a KlaviyoAPI instance if the authentication is successful.
+    """
+    klaviyo = KlaviyoAPI(API_KEY_KLAVIYO, max_delay=60, max_retries=3, test_host=None)
+    return klaviyo
+
