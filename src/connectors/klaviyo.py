b90df0c4ba51a6b16526c8154b6a023b1ef88775
Sherwin Varghese <sherwin.varghese@neenopal.com>
2025-03-08 13:37:30 +0530

    removed function and chose global setup for klaviyo object

--- src/connectors/klaviyo.py
+++ src/connectors/klaviyo.py
@@ -9,16 +9,5 @@ from klaviyo_api import KlaviyoAPI
 load_dotenv("./src/config/.env")
 API_KEY_KLAVIYO = os.environ.get("API_KEY_KLAVIYO")
 
-def init_klaviyo() -> Any:
-    """
-    Authenticate with the Klaviyo API.
-
-    This function serves as a general template to initialize the Klaviyo API connection.
-    It requires an API key for authorization and returns a KlaviyoAPI instance.
-
-    Returns:
-        Any: The function returns a KlaviyoAPI instance if the authentication is successful.
-    """
-    klaviyo = KlaviyoAPI(API_KEY_KLAVIYO, max_delay=60, max_retries=3, test_host=None)
-    return klaviyo
+klaviyo = KlaviyoAPI(API_KEY_KLAVIYO, max_delay=60, max_retries=3, test_host=None)  
 
