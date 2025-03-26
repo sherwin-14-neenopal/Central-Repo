546848d72f3e39e546c86c6a0dbed15bfa6b9328
Sherwin Varghese <sherwin.varghese@neenopal.com>
2025-03-26 18:14:18 +0530

    added figlet animation for successful completion of conversion

--- notebook_to_scripts.ps1
+++ notebook_to_scripts.ps1
@@ -12,11 +12,16 @@ $outputFile = Join-Path -Path $outputDirectory -ChildPath (Get-Item $notebookPat
 
 try {
     $conversionResult = jupyter nbconvert --to script $notebookPath --output $outputFile 2>&1
-    Write-Host "Conversion output: $conversionResult"
-    Write-Host "============================================================"
-    
+
     if (Test-Path -Path ($outputFile + ".py")) {
-        Write-Host "Converted $($notebookPath) to $($outputFile)"
+        Write-Host "+--- basic
+        d8888b.  .d88b.  d8b   db d88888b 
+        88  `8D .8P  Y8. 888o  88 88'     
+        88   88 88    88 88V8o 88 88ooooo 
+        88   88 88    88 88 V8o88 88~~~~~ 
+        88  .8D `8b  d8' 88  V888 88.     
+        Y8888D'  `Y88P'  VP   V8P Y88888P 
+       "
     } else {
         Write-Host "Failed to convert $($notebookPath). Check the output above for errors."
     }
